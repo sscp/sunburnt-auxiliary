488d88c2f25df0866eb05001b81aabe5ab86187d
fightingj305 <57042994+fightingj305@users.noreply.github.com>
2024-06-25 12:02:20 -0700

    header file

--- csvtelemlog.py
+++ csvtelemlog.py
@@ -3,8 +3,9 @@ from datetime import datetime
 
 UDP_IP = "192.168.0.100"
 UDP_PORT = 10000
-CSV_HEADER = "alive_0,bus_current_0,bus_voltage_0,heatsink_temp_0,motor_temp_0,rail_15v_0,rail_3v3_0,rail_1v9_0,dsp_temp_0,error_flags_0,limit_flags_0,rx_error_count_0,tx_error_count_0,motor_velocity_0,vehicle_velocity_0,phase_current_b_0,alive_1,bus_current_1,bus_voltage_1,heatsink_temp_1,motor_temp_1,rail_15v_1,rail_3v3_1,rail_1v9_1,dsp_temp_1,error_flags_1,limit_flags_1,rx_error_count_1,tx_error_count_1,motor_velocity_1,vehicle_velocity_1,phase_current_b_1"
-
+CSV_HEADER = ""
+with open("headers.txt","r") as file:
+    CSV_HEADER = ",".join(list(map(lambda x:x.strip("\n "),file.readlines())))
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
